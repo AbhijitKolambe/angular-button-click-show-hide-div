8d54acd346d7346b9213608e5d82f275a096f398
AbhijitKolambe <abhijitkolambe63@gmail.com>
2022-03-20 11:40:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-gtomkk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-gtomkk)
---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  div1:boolean=true;
+  div2:boolean=true;
+  div3:boolean=true;
+
+  div1Function(){
+      this.div1=true;
+      this.div2=false;
+      this.div3=false
+  }
+
+  div2Function(){
+      this.div2=true;
+      this.div1=false;
+      this.div3=false
+  }
+
+  div3Function(){
+      this.div3=true;
+      this.div2=false;
+      this.div1=false
+  }
+}
